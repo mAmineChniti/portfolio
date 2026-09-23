44ae1d87a6c71fd614e4e1f79f6306adfa6e96dc
mAmineChniti <emin.chniti@esprit.tn>
2026-09-24 00:54:27 +0100

    toggle mode

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -5,34 +5,19 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger render={<Button variant="outline" size="icon" />}>
-        <Sun className="scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90 size-[1.2rem]" />
-        <Moon className="absolute scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0 size-[1.2rem]" />
-        <span className="sr-only">Toggle theme</span>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+    >
+      <Sun className="scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90 size-[1.2rem]" />
+      <Moon className="absolute scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0 size-[1.2rem]" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   );
 }
